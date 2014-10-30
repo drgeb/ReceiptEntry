4574fdaa685b6a89a9db01582f4335c56d9f39c8
Dr. Gerald E. Bennett <developer@drgeb.com>
2014-10-30 05:53:56 -0400

    Added logging using Log4j framework
    Added dependencies and code for introducing logging to the application
    using log4j

--- src/main/java/com/drgeb/receiptentry/App.java
+++ src/main/java/com/drgeb/receiptentry/App.java
@@ -1,9 +1,13 @@
 package com.drgeb.receiptentry;
+
 /**
-*
-* @author Dr. Gerald E. Bennett
-* 
-**/
+ *
+ * @author Dr. Gerald E. Bennett
+ * 
+ **/
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.airhacks.afterburner.injection.Injector;
 import com.drgeb.receiptentry.vw.receipttable.Receipttable;
@@ -14,27 +18,30 @@ import javafx.stage.Stage;
 
 public class App extends Application {
 
-	@Override
+    private static Logger logger = LogManager.getLogger(App.class);
+
+    @Override
     public void start(Stage stage) throws Exception {
-    	//List<String> parameters = this.getParameters().getRaw();
-    	//String [] args= parameters.toArray(new String[parameters.size()]);
-    	
-    	Receipttable appView = new Receipttable();
-    	
-        Scene scene = new Scene(appView.getView());
-        stage.setTitle("GINGERLICEUOS RECEIPT ENTRY");
-        final String uri = getClass().getResource("app.css").toExternalForm();
-        scene.getStylesheets().add(uri);
-        stage.setScene(scene);
-        stage.show();
+	logger.info("Starting Application");
+	// List<String> parameters = this.getParameters().getRaw();
+	// String [] args= parameters.toArray(new String[parameters.size()]);
+
+	Receipttable appView = new Receipttable();
+
+	Scene scene = new Scene(appView.getView());
+	stage.setTitle("GINGERLICEUOS RECEIPT ENTRY");
+	final String uri = getClass().getResource("app.css").toExternalForm();
+	scene.getStylesheets().add(uri);
+	stage.setScene(scene);
+	stage.show();
     }
 
     @Override
     public void stop() throws Exception {
-        Injector.forgetAll();
+	Injector.forgetAll();
     }
 
     public static void main(String[] args) {
-        launch(args);
+	launch(args);
     }
 }
